8ef2f81a8751255da4498f96ab41db489f8d03d8
rumeone <yuaroslav_29@mail.ru>
2021-11-22 02:13:40 +0700

    Added button functionality

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,15 +1,46 @@
 class Products {
 
+    constructor() {
+        this.classNameActive = 'products-element__btn_active';
+        this.labelAdd = 'Добавить в корзину';
+        this.labelRmove = 'Удалить из корзины';
+    }
+
+    handleSetLocationStorage(element, id) {
+        const {pushProduct, products} = localStorageUtil.putProducts(id);
+
+        if (pushProduct === true) {
+            element.classList.add(this.classNameActive);
+            element.innerHTML =  this.labelRmove;
+        }
+        else {
+            element.classList.remove(this.classNameActive);
+            element.innerHTML =  this.labelAdd;
+        }
+    }
+
     render() {
+        const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
         CATALOG.forEach(({id, name, price, img}) => {
+            let activeClass = '';
+            let activeText = '';
+
+            if(productsStore.indexOf(id) === -1) {
+                activeText = this.labelAdd ;
+            }
+            else {
+                activeClass = ' ' + this.classNameActive;
+                activeText = this.labelRmove;
+            }
+
             htmlCatalog += `
             <li class="products-element">
                 <span class="products-element__name">${name}</span>
                 <img class="products-element__img" src = "${img}"/>
                 <span class="products-element__price">
                 ⚡ ${price.toLocaleString('ru-RU')} RUB</span>
-                <button class="products-element__btn">Добавить в корзину</button>
+                <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}')">${activeText}</button>
             </li>
             `;
         })
